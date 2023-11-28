213c0553c15a1b3c201b495a692662251a5957bd
Yaseen Jeber <66650758+yaseenj96@users.noreply.github.com>
2023-11-27 22:58:09 -0600

    Create Randomization.py

---
+++ Randomization.py
@@ -0,0 +1,10 @@
+import random
+# import my_module
+
+c = random.randint(100,200) 
+# Randint returns a random number between two inputs
+rand_float = random.random()*5
+# Random outputs random float
+print(c)
+# print(my_module.pi)
+print(rand_float)
