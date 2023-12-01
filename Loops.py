0547fbfc2d854e12264d3c8a9b3dfde7103490aa
Yaseen Jeber <66650758+yaseenj96@users.noreply.github.com>
2023-11-30 23:59:53 -0600

    Update Loops.py

--- Loops.py
+++ Loops.py
@@ -16,3 +16,10 @@ print(f"The highest score in the class is: {highest_score}")
 #Range of numbers between 1 and 11, incrementing by 3
 for number in range(1, 11, 3):
   print(number)
+
+#Sum of all even numbers in range:
+target = 52
+sum = 0
+for num in range(0, target+1):
+  if num % 2 == 0:
+    sum += num
