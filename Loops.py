33f1f8393c97e1d2fd463f059349198ea979fe45
Yaseen Jeber <66650758+yaseenj96@users.noreply.github.com>
2023-11-30 23:57:04 -0600

    Update Loops.py

--- Loops.py
+++ Loops.py
@@ -3,3 +3,16 @@ fruits = ["Apple", "Peach", "Pear"]
 for fruit in fruits:
   print(fruit)
   print(fruit + "Pie")
+
+student_score = [23, 34, 45, 23, 12]
+
+highest_score = 0
+for score in student_scores:
+  if score > highest_score:
+    highest_score = score
+
+print(f"The highest score in the class is: {highest_score}")
+
+#Range of numbers between 1 and 11, incrementing by 3
+for number in range(1, 11, 3):
+  print(number)
