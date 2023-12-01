a6bbed781e0b566b9ce0bf8370ca512cebc9d3dc
Yaseen Jeber <66650758+yaseenj96@users.noreply.github.com>
2023-12-01 00:04:17 -0600

    Update Loops.py

--- Loops.py
+++ Loops.py
@@ -23,3 +23,14 @@ sum = 0
 for num in range(0, target+1):
   if num % 2 == 0:
     sum += num
+
+#FizzBuzz
+for num in range(1, 101):
+  if num % 3 == 0 and num % 5 == 0:
+    print("FizzBuzz")
+  elif num % 3 == 0:
+    print("Fizz")
+  elif num % 5 == 0:
+    print("Buzz")
+  else:
+    print(num)
