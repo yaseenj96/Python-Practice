ee691d2e2d4e7d1568e5d7aa55bb15bd7af5d719
Yaseen Jeber <66650758+yaseenj96@users.noreply.github.com>
2023-12-03 22:46:49 -0600

    Update Functions_and_Karel.py

--- Functions_and_Karel.py
+++ Functions_and_Karel.py
@@ -1,1 +1,22 @@
+#Reeborg's solution:
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def turn_around():
+    turn_left()
+    turn_left()
 
+    
+while not at_goal():
+    if front_is_clear():
+        move()
+    elif wall_in_front() and right_is_clear():
+        turn_right()
+    elif wall_in_front():
+        turn_left()
+    elif front_is_clear() and right_is_clear():
+        turn_right()
+    else:
+        move()
