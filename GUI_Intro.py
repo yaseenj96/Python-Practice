d352c3aa05a9332772ae91c397bc243beb1f0278
Yaseen Jeber <66650758+yaseenj96@users.noreply.github.com>
2024-01-24 00:08:23 -0600

    Create GUI_Intro.py
    TKinter GUI widgets

---
+++ GUI_Intro.py
@@ -0,0 +1,129 @@
+from tkinter import *
+FONT = ("Arial", 12)
+
+window = Tk()
+window.title("Mile to Km Converter")
+window.minsize(width=200,height=100)
+window.config(padx=20,pady=10)
+
+#Labels
+
+miles = Label(text="Miles", font= FONT)
+miles.grid(column=2, row=0)
+
+is_equal_to = Label(text="is equal to", font= FONT)
+is_equal_to.grid(column=0,row=1)
+
+km = Label(text="Km", font= FONT)
+km.grid(column=2,row=1)
+
+#Entry
+input = Entry(width= 5)
+input.grid(column=1,row=0)
+
+#Text
+text = Label(text="0", font=FONT)
+text.grid(column=1,row=1)
+
+#Button
+def button_clicked():
+    km_out = int(input.get()) * 1.609
+    text["text"] = km_out
+
+button = Button(text= "Calculate", command=button_clicked)
+button.grid(column=1, row=2)
+
+window.mainloop()
+
+
+###
+##
+## Additional TKinter Widgets
+##
+##
+##
+from tkinter import *
+
+#Creating a new window and configurations
+window = Tk()
+window.title("Widget Examples")
+window.minsize(width=500, height=500)
+
+#Labels
+label = Label(text="This is old text")
+label.config(text="This is new text")
+label.pack()
+
+#Buttons
+def action():
+    print("Do something")
+
+#calls action() when pressed
+button = Button(text="Click Me", command=action)
+button.pack()
+
+#Entries
+entry = Entry(width=30)
+#Add some text to begin with
+entry.insert(END, string="Some text to begin with.")
+#Gets text in entry
+print(entry.get())
+entry.pack()
+
+#Text
+text = Text(height=5, width=30)
+#Puts cursor in textbox.
+text.focus()
+#Adds some text to begin with.
+text.insert(END, "Example of multi-line text entry.")
+#Get's current value in textbox at line 1, character 0
+print(text.get("1.0", END))
+text.pack()
+
+#Spinbox
+def spinbox_used():
+    #gets the current value in spinbox.
+    print(spinbox.get())
+spinbox = Spinbox(from_=0, to=10, width=5, command=spinbox_used)
+spinbox.pack()
+
+#Scale
+#Called with current scale value.
+def scale_used(value):
+    print(value)
+scale = Scale(from_=0, to=100, command=scale_used)
+scale.pack()
+
+#Checkbutton
+def checkbutton_used():
+    #Prints 1 if On button checked, otherwise 0.
+    print(checked_state.get())
+#variable to hold on to checked state, 0 is off, 1 is on.
+checked_state = IntVar()
+checkbutton = Checkbutton(text="Is On?", variable=checked_state, command=checkbutton_used)
+checked_state.get()
+checkbutton.pack()
+
+#Radiobutton
+def radio_used():
+    print(radio_state.get())
+#Variable to hold on to which radio button value is checked.
+radio_state = IntVar()
+radiobutton1 = Radiobutton(text="Option1", value=1, variable=radio_state, command=radio_used)
+radiobutton2 = Radiobutton(text="Option2", value=2, variable=radio_state, command=radio_used)
+radiobutton1.pack()
+radiobutton2.pack()
+
+
+#Listbox
+def listbox_used(event):
+    # Gets current selection from listbox
+    print(listbox.get(listbox.curselection()))
+
+listbox = Listbox(height=4)
+fruits = ["Apple", "Pear", "Orange", "Banana"]
+for item in fruits:
+    listbox.insert(fruits.index(item), item)
+listbox.bind("<<ListboxSelect>>", listbox_used)
+listbox.pack()
+window.mainloop()
